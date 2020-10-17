849bdc9fd69b6e17a1f91dd996bd9181bff729aa
jayanth <jayanthkaturi72@gmail.com>
2020-10-18 04:39:16 +0530

    Updates

---
+++ precache-manifest.706e0f532a11a7c1c135e69d93427279.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "314690e4576674fe53bf99b52d06b096",
+    "url": "/halloween/index.html"
+  },
+  {
+    "revision": "5ed397d61b49f233e590",
+    "url": "/halloween/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "806d3f3735cc82d22eb4",
+    "url": "/halloween/static/js/2.4496f3d0.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/halloween/static/js/2.4496f3d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ed397d61b49f233e590",
+    "url": "/halloween/static/js/main.50d33fc2.chunk.js"
+  },
+  {
+    "revision": "c0e2b6f95fd176e7a05a",
+    "url": "/halloween/static/js/runtime-main.0f05412f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/halloween/static/media/logo.5d5d9eef.svg"
+  }
+]);
